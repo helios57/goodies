bab277476b45c01f3510fc13c7a13143fede867e
Gilles Iachelini <gilles.iachelini@mtrail.de>
2019-08-13 18:07:07 +0200

    initial commit - adding handler

---
+++ de.mtrail.goodies/src/de/mtrail/goodies/internal/workspacesupport/CreateWorkspacePropertiesHandler.java
@@ -0,0 +1,15 @@
+package de.mtrail.goodies.internal.workspacesupport;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+
+public final class CreateWorkspacePropertiesHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		return null;
+	}
+}
